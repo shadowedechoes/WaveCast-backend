fe3956f61856f430f5a1198f893019a281d45cc8
shadowedechoes <sebby2012a@gmail.com>
2026-06-17 23:04:29 +1000

    Add files via upload

---
+++ WaveCast-backend/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+
+    passwordHash: {
+        type: String,
+        required: true
+    },
+
+    role: {
+        type: String,
+        default: "listener"
+    },
+
+    banned: {
+        type: Boolean,
+        default: false
+    }
+
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("User", userSchema);
