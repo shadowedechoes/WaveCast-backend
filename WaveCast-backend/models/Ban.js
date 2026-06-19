fe31a943fe9f61e52de79a075898128ea8c9c75d
shadowedechoes <sebby2012a@gmail.com>
2026-06-19 10:30:19 +1000

    Delete WaveCast-backend directory

--- WaveCast-backend/models/Ban.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("Ban", {
-    username: String,
-    reason: String
-});
--- WaveCast-backend/models/Booking.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("Booking", {
-    station: String,
-    dj: String,
-    time: String
-});
